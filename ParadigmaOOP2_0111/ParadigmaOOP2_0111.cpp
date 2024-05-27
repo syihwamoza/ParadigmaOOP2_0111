5397f9b9139072d056902375028bff382e8a43b4
syihwamoza <mozakastella26@gmail.com>
2024-05-27 10:03:30 +0700

    implementasi kelas turunan joko

--- ParadigmaOOP2_0111/ParadigmaOOP2_0111.cpp
+++ ParadigmaOOP2_0111/ParadigmaOOP2_0111.cpp
@@ -4,4 +4,14 @@ using namespace std;
 class seseorang {
 public:
 	virtual void pesan() = 0;
+	/*virtual void pesan(){
+	    cout<<"Pesan dari seseorang"<<endl;
+	}*/
+};
+
+class joko :public seseorang {
+public:
+	void pesan() {
+		cout << "Pesan dari joko" << endl;
+	}
 };
